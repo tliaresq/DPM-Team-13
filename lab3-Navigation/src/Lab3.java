2383d4081a84fd4f1745e0ba319d27a83567bbca
Cedric <cedric.bonjour@hotmail.fr>
2015-02-02 13:25:47 -0500

    lab3 follows the set path as asked

--- lab3-Navigation/src/Lab3.java
+++ lab3-Navigation/src/Lab3.java
@@ -1,26 +1,22 @@
 import lejos.nxt.*;
 
 public class Lab3 {
-	static int xDest = 60;
-	static int yDest = -30;
 	static double leftRadius = 2.1;
 	static double rightRadius = 2.1;
 	static double width = 14.70;
-	
-	
+
 	public static void main(String[] args) {
 		Odometer odometer = new Odometer();
 		OdometryDisplay odometryDisplay = new OdometryDisplay(odometer);
-
+		Navigate nav = new Navigate(Motor.A, Motor.B, leftRadius, rightRadius,
+				width, odometer, odometryDisplay);
 		odometer.start();
 		odometryDisplay.start();
-		(new Thread() {
-			public void run() {
-						Navigate.drive(xDest, yDest, Motor.A, Motor.B, leftRadius, rightRadius, width);		
-			}
-		}).start();
+		nav.start();
 		
-		while (Button.waitForAnyPress() != Button.ID_ESCAPE); // error in origin file : used to be "Button.waitForPress()"
-		System.exit(0);	
+
+		while (Button.waitForAnyPress() != Button.ID_ESCAPE)
+			; // error in origin file : used to be "Button.waitForPress()"
+		System.exit(0);
 	}
 }
--- lab3-Navigation/src/Navigate.java
+++ lab3-Navigation/src/Navigate.java
@@ -1,113 +1,98 @@
-import lejos.nxt.Motor;
 import lejos.nxt.NXTRegulatedMotor;
 
-public class Navigate {
+public class Navigate extends Thread {
 	private static final int FORWARD_SPEED = 200;
 	private static final int ROTATE_SPEED = 150;
 	private static NXTRegulatedMotor leftMotor;
 	private static NXTRegulatedMotor rightMotor;
 	private static double rightRadius;
 	private static double leftRadius;
-	private static int xDest;
-	private static int yDest;
+	private static double xDest;
+	private static double yDest;
 	private static double width;
-	
-	public static void drive(int x, int y, NXTRegulatedMotor lMotor,
-			NXTRegulatedMotor rMotor, double lRadius,
-			double rRadius, double w) {
-		
-		double distanceLeft;
-		leftMotor=lMotor;
-		rightMotor=rMotor;
-		leftRadius= lRadius;
-		rightRadius= rRadius;
-		xDest=x;
-		yDest=y;
+	private static Odometer odometer;
+	private static OdometryDisplay odometryDisplay;
+	private static boolean isNavigating;
+
+	public Navigate(NXTRegulatedMotor lMotor, NXTRegulatedMotor rMotor,
+			double lRadius, double rRadius, double w, Odometer odo,
+			OdometryDisplay display) {
+		new Object();
+		leftMotor = lMotor;
+		rightMotor = rMotor;
+		leftRadius = lRadius;
+		rightRadius = rRadius;
 		width = w;
+		odometer = odo;
+		odometryDisplay = display;
+		isNavigating= false;
+	}
+
+	public void run() {
+		travelTo(60,30);
+
+		travelTo(30,30);
+
+		travelTo(30,60);
 		
-		
-		
-		
-		for (NXTRegulatedMotor motor : new NXTRegulatedMotor[] { leftMotor, rightMotor }) {
+		travelTo(60,0);
+	}
+
+	public static void travelTo(double x, double y) {
+		isNavigating = true;
+		xDest = x;
+		yDest = y;
+
+		for (NXTRegulatedMotor motor : new NXTRegulatedMotor[] { leftMotor,
+				rightMotor }) {
 			motor.stop();
 			motor.setAcceleration(3000);
 		}
 
-		// wait 5 seconds
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			// there is nothing to be done here because it is not expected that
-			// the odometer will be interrupted by another thread
-		}
-
-			Odometer odometer = new Odometer();
-			OdometryDisplay odometryDisplay = new OdometryDisplay(odometer);
-			
-			
-			
-			directToDest(odometer);
-			
-			travelDist(distToDest(odometer));
-			
-			leftMotor.setSpeed(0);
-			rightMotor.setSpeed(0);
+	
 		
-			
-			//leftMotor.rotate(convertDistance(leftRadius, 60.96), true);
-			//rightMotor.rotate(convertDistance(rightRadius, 60.96), false);
-			
-
-
+		turnTo();
+		travelDist(distToDest());
+		isNavigating = false;
 	}
-	
-	
-	private static double distToDest(Odometer odometer){
+
+	private static double distToDest() {
 		double distance;
-		
-		double y= Math.abs(odometer.getY()-yDest);
-		double x= Math.abs(odometer.getX()-xDest);
-		
-		distance = Math.sqrt(Math.pow(x, 2)+Math.pow(y, 2));
-		
+
+		double y = Math.abs(odometer.getY() - yDest);
+		double x = Math.abs(odometer.getX() - xDest);
+
+		distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+
 		odometer.setV(distance);
-		
-		
+
 		return distance;
 	}
-	
-	
 
-	private static void directToDest(Odometer odometer) {
-		
-		double angle = Math.atan2(yDest,xDest)*180/3.14159;
-		double theta= odometer.getTheta();
-		
-		if(theta-angle<=180 ){
-			angle = theta-angle;
-		}else{
-			angle = theta+angle;
+	private static void turnTo() {
+
+		double angle = Math.atan2(yDest-odometer.getY(), xDest-odometer.getX()) * 180 / 3.14159;
+		double theta = odometer.getTheta();
+
+		if (theta - angle <= 180) {
+			angle = theta - angle;
+		} else {
+			angle = theta + angle;
 		}
-		
-		
+
 		leftMotor.setSpeed(ROTATE_SPEED);
 		rightMotor.setSpeed(ROTATE_SPEED);
 
 		leftMotor.rotate(convertAngle(leftRadius, width, angle), true);
 		rightMotor.rotate(-convertAngle(rightRadius, width, angle), false);
-
-		
-		
 	}
-	
-	private static void travelDist(double distance){
 
+	private static void travelDist(double distance) {
 
 		leftMotor.rotate(convertDistance(leftRadius, distance), true);
 		rightMotor.rotate(convertDistance(rightRadius, distance), false);
 
 	}
-
 
 	private static int convertDistance(double radius, double distance) {
 		return (int) ((180.0 * distance) / (Math.PI * radius));
@@ -115,5 +100,8 @@ public class Navigate {
 
 	private static int convertAngle(double radius, double width, double angle) {
 		return convertDistance(radius, Math.PI * width * angle / 360.0);
+	}
+	public boolean isNavigating(){
+		return isNavigating;
 	}
 }
